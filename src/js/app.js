33bb0ca70bfca83df40179c613a067ed4bfb28b8
SadMearise <65180406+SadMearise@users.noreply.github.com>
2022-02-14 13:55:01 +0300

    1

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as sl from "./slick.min.js";
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+flsFunctions.ibg();
+flsFunctions.toggleClassActive();
+flsFunctions.sliderSlick();
